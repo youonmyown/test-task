fbcbbe2370fefafe929fe5b7b574514b870caa90
youonmyown <1337onmyown@gmail.com>
2024-09-13 13:35:10 +0300

    add ssh connections, output string

--- main.tf
+++ main.tf
@@ -18,8 +18,34 @@ provider "aws" {
 resource "aws_instance" "test_server" {
   ami           = "ami-0584590e5f0e97daa"
   instance_type = "t2.micro"
+  key_name      = "griga-key"
+
+  vpc_security_group_ids = [aws_security_group.ssh_access.id]
 
   tags = {
     Name = "GrigaTask"
+  }
+}
+
+resource "aws_security_group" "ssh_access" {
+  name        = "allow_ssh"
+  description = "Allow SSH access from anywhere"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # allow 22 port
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # allow all traffic
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "AllowSSH"
   }
 }
--- output.tf
+++ output.tf
@@ -2,3 +2,8 @@ output "instance_public_ip" {
   description = "The public IP of the EC2 instance"
   value       = aws_instance.test_server.public_ip
 }
+
+output "ssh_connection_string" {
+  description = "SSH connection string to connect to the EC2 instance"
+  value       = "ssh -i "griga-key.pem" admin@${aws_instance.test_server.public_ip}"
+}
