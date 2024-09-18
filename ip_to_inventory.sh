8da9712cdbc1334a173882f0bcb0dafda570b709
youonmyown <1337onmyown@gmail.com>
2024-09-18 15:44:42 +0300

    update ip to ansible

--- ip_to_inventory.sh
+++ ip_to_inventory.sh
@@ -2,5 +2,4 @@
 
 elastic_ip=$(terraform output -raw elastic_ip)
 
-echo "[webserver]" > hosts
-echo "$elastic_ip ansible_python_interpreter=/usr/bin/python3" >> hosts
+ansible-playbook -i $elastic_ip site.yml
