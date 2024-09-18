4b2196a19a08620f6d4d6574e6533af0203b9185
youonmyown <1337onmyown@gmail.com>
2024-09-18 11:33:53 +0300

    fix ip to hosts sctips, test run

--- ip_to_inventory.sh
+++ ip_to_inventory.sh
@@ -3,4 +3,4 @@
 elastic_ip=$(terraform output -raw elastic_ip)
 
 echo "[webserver]" > hosts
-echo "\"$elastic_ip\" ansible_python_interpreter=/usr/bin/python3" >> hosts
+echo "$elastic_ip ansible_python_interpreter=/usr/bin/python3" >> hosts
